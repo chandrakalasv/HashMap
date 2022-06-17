aea3664bfa707fbd3b6c638d3eac5404b1702fb7
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-18 00:24:54 +0530

    added code to find frequency of a words in sentence

--- src/com/bridgelab/hashmap/HashMapMain.java
+++ src/com/bridgelab/hashmap/HashMapMain.java
@@ -2,6 +2,19 @@ package com.bridgelab.hashmap;
 
 public class HashMapMain {
     public static void main(String[] args) {
-        System.out.println("Hash Map implementation");
+        String sentence = "to be or not to be";
+        IHashMap<String,Integer> hashMap = new HashMap<>();
+        String[] words = sentence.toLowerCase().split(" ");
+        for(String word: words) {
+            Integer value = hashMap.get(word);
+            if(value == null)
+                value = 1;
+            else
+                value = value + 1;
+            hashMap.put(word, value);
+        }
+        int frequency = hashMap.get("to");
+        System.out.println(frequency);
+        System.out.println(hashMap);
     }
 }
---
+++ src/com/bridgelab/hashmap/IHashMap.java
@@ -0,0 +1,6 @@
+package com.bridgelab.hashmap;
+
+public interface IHashMap<K, T> {
+    void put(K key, T data);
+    T get(K key);
+}
